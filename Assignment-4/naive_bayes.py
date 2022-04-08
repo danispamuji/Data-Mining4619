7e695e32f439cc21497b908fc838f65cdb727f66
danispamuji <80206604+danispamuji@users.noreply.github.com>
2022-04-08 23:54:35 +0700

    Add files via upload

---
+++ Assignment-4/naive_bayes.py
@@ -0,0 +1,7 @@
+import rumahsakit as rs
+import numpy as np
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_rows', 9999)
+pd.set_option('display.max_columns', 9999)
+pd.set_option('display.width', 9999)
